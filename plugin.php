20f5f69f8289b6832329e1bac3940c261b13f984
Aaron <clonemeagain@gmail.com>
2016-05-14 08:12:52 +1000

    Fixing url

--- plugin.php
+++ plugin.php
@@ -6,6 +6,6 @@ return array(
     'name' => 'Ticket View: Indicate Not Yours',
     'author' => 'clonemeagain@gmail.com',
     'description' => 'When viewing a ticket, Garishly restyles the page if viewing someone elses ticket. Relies on attachment_preview plugin.',
-    'url' => 'https://github.com/clonemeagain/osticket-note-first-plugin',
+    'url' => 'https://github.com/clonemeagain/osticket-not-yours-plugin',
     'plugin' => 'IndicateNotYoursPlugin.php:IndicateNotYoursPlugin'
 );
